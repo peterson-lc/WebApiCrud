f5a8ab90e0d3d08467924e8e359dcdc478a9bdf0
peterson.cardoso <peterson.cardoso@sequor.com.br>
2019-05-08 23:29:06 -0300

    Added Display name in models

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,23 @@ namespace WebApiCrud.Models
     public class Cliente
     {
         public int Id { get; set; }
+
+        [DisplayName("Razão Social")]
         public string RazaoSocial { get; set; }
+
+        [DisplayName("Nome Fantasia")]
         public string NomeFantasia { get; set; }
+
+        [DisplayName("E-mail")]
         public string Email { get; set; }
+
+        [DisplayName("CNPJ")]
         public string Cnpj { get; set; }
+
+        [DisplayName("Telefone")]
         public string Telefone { get; set; }
+
+        [DisplayName("Endereço")]
         public string Endereco { get; set; }
     }
 }
